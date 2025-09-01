1eb86472aa923fa290d133bbb249ff492083101d
Pedro-0101 <pedro.tiete@outlook.com>
2025-09-01 20:33:57 -0300

    feat(test): adicionado mais testes unitarios para address [#6]

--- src/tests/address/address.spec.ts
+++ src/tests/address/address.spec.ts
@@ -2,22 +2,24 @@ import { describe, it, expect } from "vitest";
 import { validate } from "class-validator";
 import { createAddressDto } from '../../dtos/address/addressDtos'
 
+
+const country = 'Brasil'
+const state = 'SP'
+const city = 'Tiete'
+const cep = '18530-000'
+const neighbor = 'Centro'
+const street = 'Avenida sao joao'
+const number = 122
+const addicional = "Teste"
+const id = 1
+
+var address = new createAddressDto(
+    { id, country, state, city, cep, neighbor, street, number, addicional }
+)
+
+
 describe('Create address dto', async () => {
     it('Cria um endereco valido', async () => {
-
-        const country = 'Brasil'
-        const state = 'SP'
-        const city = 'Tiete'
-        const cep = '18530-000'
-        const neighbor = 'Centro'
-        const street = 'Avenida sao joao'
-        const number = 122
-        const addicional = "Teste"
-        const id = 1
-
-        const address = new createAddressDto(
-            {id, country, state, city, cep, neighbor, street, number, addicional}
-        )
 
         const errors = await validate(address)
         console.log(errors)
@@ -33,5 +35,27 @@ describe('Create address dto', async () => {
         expect(address.street).toBe(street)
         expect(address.number).toBe(number)
         expect(address.addicional).toBe(addicional)
+    })
+
+    it('Nao cria endereco com nome de pais menor que 3', async () => {
+
+        address.country = 'br'
+
+        const errors = await validate(address)
+        
+        expect(errors.length).toBe(1)
+        expect(errors.map( (e) => e.property)).toContain('country')
+
+    })
+
+    it('Nao cria endereco com nome de pais maior que 50', async () => {
+
+        address.country = 'nomeInvalidoDePaisnomeInvalidoDePaisnomeInvalidoDePaisnomeInvalidoDePaisnomeInvalidoDePaisnomeInvalidoDePaisnomeInvalidoDePais'
+
+        const errors = await validate(address)
+        
+        expect(errors.length).toBe(1)
+        expect(errors.map( (e) => e.property)).toContain('country')
+
     })
 })
