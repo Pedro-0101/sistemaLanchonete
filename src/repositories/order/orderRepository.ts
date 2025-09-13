7755d58b9e4ea6c0ee2ee047b6565b2986d12d97
pedro-0101 <pedro.tiete@outlook.com>
2025-09-13 09:30:09 -0300

    feat(repo): criado repositorios para classe order e usesr [#27]

--- src/repositories/order/orderRepository.ts
+++ src/repositories/order/orderRepository.ts
@@ -7,7 +7,61 @@ export interface orderInterface {
   save(order: Order): Promise<Order>;
   list(): Promise<Order[]>;
   update(order: Order): Promise<Order>;
-  delete(id: number): Promise<void>;
-  listByUser(userId: number): Promise<Order[]>;
-  getById(id: number): Promise<Order>;
+  delete(id: string): Promise<void>;
+  getById(id: string): Promise<Order>;
+}
+
+export class OrderRepository implements orderInterface {
+  async save(order: Order): Promise<Order> {
+    const savedOrder = await prisma.order.create({
+      data: {
+        id: order.id,
+        user_id: order.user.id,
+        establishment_id: order.establishment.id,
+        payment_method_id: order.paymentMethod.id,
+        delivery_type_id: order.deliveryType.id,
+        status_id: order.status.id,
+        sent_date: order.sentDate,
+        delivery_date: order.deliveryDate,
+      },
+    });
+    return Order.create(savedOrder);
+  }
+
+  async list(): Promise<Order[]> {
+    const orders = await prisma.order.findMany();
+    return orders.map((o) => Order.create(o));
+  }
+
+  async update(order: Order): Promise<Order> {
+    const updatedOrder = await prisma.order.update({
+      where: { id: order.id },
+      data: {
+        id: order.id,
+        user_id: order.user.id,
+        establishment_id: order.establishment.id,
+        payment_method_id: order.paymentMethod.id,
+        delivery_type_id: order.deliveryType.id,
+        status_id: order.status.id,
+        sent_date: order.sentDate,
+        delivery_date: order.deliveryDate,
+      },
+    });
+    return Order.create(updatedOrder);
+  }
+
+  async delete(id: string): Promise<void> {
+    await prisma.order.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async getById(id: string): Promise<Order> {
+    const order = await prisma.order.findUnique({
+      where: { id: id },
+    });
+    return Order.create(order);
+  }
 }
--- src/repositories/user/userRepository.ts
+++ src/repositories/user/userRepository.ts
@@ -7,6 +7,55 @@ export interface userInterface {
   save(user: User): Promise<User>;
   list(): Promise<User[]>;
   update(user: User): Promise<User>;
-  delete(id: number): Promise<void>;
-  getById(id: number): Promise<User>;
+  delete(id: string): Promise<void>;
+  getById(id: string): Promise<User>;
+}
+
+export class UserReposotiry implements userInterface {
+  async save(user: User): Promise<User> {
+    const savedUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        contact_number_id: user.contactNumber.id,
+        address_id: user.address.id,
+        status_id: user.status.id,
+      },
+    });
+    return User.create(savedUser);
+  }
+
+  async list(): Promise<User[]> {
+    const users = await prisma.user.findMany();
+    return users.map((u) => User.create(u));
+  }
+
+  async update(user: User): Promise<User> {
+    const updatedUser = await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        contact_number_id: user.contactNumber.id,
+        address_id: user.address.id,
+        status_id: user.status.id,
+      },
+    });
+    return User.create(updatedUser);
+  }
+
+  async delete(id: string): Promise<void> {
+    await prisma.user.delete({
+      where: { id: id },
+    });
+  }
+
+  async getById(id: string): Promise<User> {
+    const user = await prisma.user.findUnique({
+      where: { id: id },
+    });
+    return User.create(user);
+  }
 }
